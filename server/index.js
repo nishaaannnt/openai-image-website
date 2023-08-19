a77fd6eb08f4d83bd112bf06d5d053682c927378
Nishant Dixit <nd221619@gmail.com>
2023-08-19 12:21:16 +0530

    cors cchanges

--- server/index.js
+++ server/index.js
@@ -2,18 +2,20 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./mongodb/connect.js";
-
 import postRoutes from './routes/postRoutes.js'
 import userRegister from './routes/userRegister.js'
 import dalleRoutes from './routes/dalleRoutes.js'
 import loginRoutes from './routes/loginRoute.js'
 
-
-
 dotenv.config();
 
+
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin:"https://openai-image-website.vercel.app/",
+}));
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use('/api/v1/post',postRoutes)
