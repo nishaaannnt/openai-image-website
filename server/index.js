b4111fe5cb67d24b0b6ff7b33969f90c85aac45c
Nishant Dixit <nd221619@gmail.com>
2023-08-19 12:27:13 +0530

    cors cchanges[2]

--- server/index.js
+++ server/index.js
@@ -12,9 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin:"https://openai-image-website.vercel.app",
-}));
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 
