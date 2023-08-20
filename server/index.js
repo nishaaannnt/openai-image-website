124873bedcb96d4c6b5b3fd16cfc1ed2ff6df89e
Nishant Dixit <nd221619@gmail.com>
2023-08-20 23:02:09 +0530

    backend changes

--- server/index.js
+++ server/index.js
@@ -12,11 +12,7 @@ dotenv.config();
 const app = express();
 
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.options('*', (req, res) => {
